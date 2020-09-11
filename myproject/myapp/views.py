c8ab10080c612311832ddec69218d703edcae7d7
youshuad <dingyoushuang@126.com>
2020-09-11 17:28:26 +0800

    Initialization

---
+++ myproject/myapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from django.views.decorators.http import require_http_methods
+from .models import Book
+import requests
+import json
+
+# Create your views here.
+@require_http_methods(["GET"])
+def add_book(request):
+    response = {}
+    try:
+        book = Book(book_name=request.GET.get('book_name'))
+        book.save()
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except  Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+    return JsonResponse(response)
+
+@require_http_methods(["GET"])
+def show_books(request):
+    response = {}
+    try:
+        books = Book.objects.filter()
+        response['list'] = json.loads(serializers.serialize("json",books))
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+
+    return JsonResponse(response)
